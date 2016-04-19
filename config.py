7b932d9807c96c4495537171901cb7f97ac65348
snowyxx <snowyxx@126.com>
2016-04-19 12:35:54 +0800

    re-init

---
+++ config.py
@@ -0,0 +1,34 @@
+#!/usr/bin/env python
+import os
+basedir = os.path.abspath(os.path.dirname(__file__))
+
+SQLALCHEMY_DATABASE_URI = 'sqlite:///' + os.path.join(basedir, 'app.db')
+SQLALCHEMY_MIGRATE_REPO = os.path.join(basedir, 'db_repository')
+SQLALCHEMY_TRACK_MODIFICATIONS = True
+
+WTF_CSRF_ENABLED = True
+SECRET_KEY = 'you-will-never-guess'
+
+OPENID_PROVIDERS = [
+    {'name': 'Google', 'url': 'https://www.google.com/accounts/o8/id'},
+    {'name': 'Yahoo', 'url': 'https://me.yahoo.com'},
+    {'name': 'AOL', 'url': 'http://openid.aol.com/<username>'},
+    {'name': 'Flickr', 'url': 'http://www.flickr.com/<username>'},
+    {'name': 'MyOpenID', 'url': 'https://www.myopenid.com'}]
+
+
+# mail server settings
+MAIL_SERVER = 'smtp.xxx.com'
+MAIL_PORT = 25
+MAIL_USERNAME = "yourid@xxx.com"
+MAIL_PASSWORD = "yourpw"
+
+# administrator list
+ADMINS = ['admin@xxx.com']
+
+
+# pagination
+POSTS_PER_PAGE = 3
+
+WHOOSH_BASE = os.path.join(basedir, 'search.db')
+MAX_SEARCH_RESULTS = 50
